694a1f25d25cd5999f936d52bd14166d843f744c
TaHiroki <git.hiroki@gmail.com>
2020-12-08 01:28:53 +0900

    第２章課題
    第２章課題提出

---
+++ 2-11/index.php
@@ -0,0 +1,24 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="text" name="my_mail" />
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+
+<?php
+  /*
+    用語：仕様書・設計書
+    仕様書はどうあるべきかという、仕様をまとめたもの。
+    設計書はその使用を元に、どんな技術でどのように構成構成するかという
+    詳しく決めたもの
+    用語：Git
+    バージョン管理のツール。特徴は分散型バージョン管理システムで、
+    リモートリポジトリからローカルへコピー、その後の変更をリモートへ
+    マージする。
+    用語：マージツール
+    差分比較やマージなどをやりやすくするためにした、開発支援ツール
+  */
+?>
