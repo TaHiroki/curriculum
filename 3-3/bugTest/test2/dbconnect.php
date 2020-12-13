eba6bcdaae696b1912cd02cfe91f0cf943c66c6d
TaHiroki <git.hiroki@gmail.com>
2020-12-13 17:39:09 +0900

    ３章課題
    ３章課題提出

---
+++ 3-3/bugTest/test2/dbconnect.php
@@ -0,0 +1,6 @@
+<?php
+//session_start();　すでに開始されている
+$db['host'] = "localhost";
+$db['user'] = "root";
+$db['pass'] = "root";//rootいれる
+$db['dbname'] = "YIGroupBlog";//名前間違え
---
+++ 3-3/bugTest/test2/index.php
@@ -0,0 +1,72 @@
+<?php
+//require 'lib/password.php'; ←いらない
+
+session_start();
+include_once("dbconnect.php");//dbInfo.phpファイル名間違い
+
+$errorMessage = "";
+$signUpMessage = "";
+
+//session_start();　すでに開始されている
+
+if (isset($_POST["signUp"])) {
+    if (empty($_POST)) {  
+        $errorMessage = 'ユーザーIDが未入力です。';
+    } else if (empty($_POST["password"])) {
+        $errorMessage = 'パスワードが未入力です。';
+    } else if (empty($_POST["password2"])) {
+        $errorMessage = 'パスワードが未入力です。';
+    }
+
+    if (!empty($_POST["username"]) && !empty($_POST["password"]) && !empty($_POST["password2"]) && $_POST["password"] === $_POST["password2"]) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
+
+        try {
+            $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+            $stmt = $pdo->prepare("INSERT INTO users(name, password) VALUES (?, ?)");
+
+            $stmt->execute(array($username, password_hash($password, PASSWORD_DEFAULT)));
+            $userid = $pdo->lastinsertid();  
+
+            $signUpMessage = '登録が完了しました。あなたの登録IDは ' . $userid . ' です。パスワードは ' . $password . ' です。';
+        } catch (PDOException $e) {
+            $errorMessage = 'データベースエラー';
+            // $e->getMessage(); //でエラー内容を参照可能（デバック時のみ表示）
+            // echo $e->getMessage();
+        }
+    } else if ($_POST["password"] != $_POST["password2"]) {
+        $errorMessage = 'パスワードに誤りがあります。';
+    }
+}
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>新規登録</title>
+    </head>
+    <body>
+        <h1>新規登録画面</h1>
+        <form id="loginForm" name="loginForm" action="" method="POST">
+            <fieldset>
+                <legend>新規登録フォーム</legend>
+                <div><font color="#ff0000"><?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?></font></div>
+                <div><font color="#0000ff"><?php echo htmlspecialchars($signUpMessage, ENT_QUOTES); ?></font></div>
+                <label for="username">ユーザー名</label><input type="text" id="username" name="username" placeholder="ユーザー名を入力" value="<?php if (!empty($_POST["username"])) {
+    echo htmlspecialchars($_POST["username"], ENT_QUOTES);
+} ?>">
+                <br>
+                <label for="password">パスワード</label><input type="password" id="password" name="password" value="" placeholder="パスワードを入力">
+                <br>
+                <label for="password2">パスワード(確認用)</label><input type="password" id="password2" name="password2" value="" placeholder="再度パスワードを入力">
+                <br>
+                <input type="submit" id="signUp" name="signUp" value="登録">
+            </fieldset>
+        </form>
+    </body>
+</html>
