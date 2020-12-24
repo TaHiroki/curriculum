2a1bee6574d18785236ff988601ca178cad70437
TaHiroki <git.hiroki@gmail.com>
2020-12-24 22:20:23 +0900

    ４章課題
    ４章課題提出

--- 4-1/function.php
+++ 4-1/function.php
@@ -8,4 +8,37 @@
         exit;
     }
   }
+
+  function redirect_main_unless_parameter($param){
+    if(empty($param)){
+      header("Location: main.php");
+      exit;
+    }
+  }
+
+  function find_post_by_id($id){
+    $pdo = db_connect();
+    try {
+      // SQL文の準備
+      $sql = "SELECT * FROM posts WHERE id = :id";
+      // プリペアドステートメントの作成
+      $stmt = $pdo->prepare($sql);
+      // idのバインド
+      $stmt->bindParam(':id', $id);
+      $stmt->execute();
+    } catch (PDOException $e) {
+      // エラーメッセージの出力
+      echo 'Error: ' . $e->getMessage();
+      // 終了
+      die();
+      //dieとexitの言語的使用は違わない
+    }
+
+    // 結果が1行取得できたら
+    if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        return $row;
+    } else {
+      redirect_main_unless_parameter($row);
+    }
+  }
 ?>
--- 4-1/main.php
+++ 4-1/main.php
@@ -30,6 +30,9 @@
         <td>タイトル</td>
         <td>本文</td>
         <td>投稿日</td>
+        <td></td>
+        <td></td>
+        <td></td>
     </tr>
     <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
         <tr>
@@ -37,6 +40,9 @@
             <td><?php echo $row['title']; ?></td>
             <td><?php echo $row['content']; ?></td>
             <td><?php echo $row['time']; ?></td>
+            <td><a href="detail_post.php?id=<?php echo $row['id']; ?>">詳細</a></td>
+            <td><a href="edit_post.php?id=<?php echo $row['id']; ?>">編集</a></td>
+            <td><a href="delete_post.php?id=<?php echo $row['id']; ?>">削除</a></td>
         </tr>
     <?php } ?>
 </table>
