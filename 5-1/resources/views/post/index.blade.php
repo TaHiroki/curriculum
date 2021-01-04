383688fa3ab6c2627719cba12af71c1163ac0dca
TaHiroki <git.hiroki@gmail.com>
2021-01-04 17:57:51 +0900

    5章課題
    ５章課題提出

--- 5-1/resources/views/post/index.blade.php
+++ 5-1/resources/views/post/index.blade.php
@@ -25,9 +25,11 @@
                     <h5 class="card-title">{{ $post->user->name }}</h5>
                     <h6 class="card-subtitle mt-2 mr-5 text-muted">{{ $post->created_at }}</h6>
                 </div>
-                    <p class="card-text">{{ $post->body }}</p>
+                <p class="card-text">{{ $post->body }}</p>
                 <div class="row d-flex justify-content-end">
-                    <button type="button" class="card-link btn btn-danger ml-4 mr-4" onclick="location.href='main.php'">削除</button>
+                    @if(Auth::id() == $post->user_id)
+                        <button type="button" class="card-link btn btn-danger ml-4 mr-4" onclick="location.href='main.php'">削除</button>
+                    @endif
                 </div>
             </div>
         </div>
